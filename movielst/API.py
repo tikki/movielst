913aaeae509e09e5a068b5cfdcf04a7d9f9044c1
Mozzo1000 <andreas@simplymozzo.se>
2018-07-19 19:05:38 +0200

    Added central API class
    Started centralize the API calls.

---
+++ movielst/API.py
@@ -0,0 +1,51 @@
+from .omdb import get_omdb_movie
+from .tmdb import get_tmdb_movie
+
+
+def get_api(title, year, external_api="omdb"):
+    item = {
+        "title": None,
+        "genre": None,
+        "imdb": None,
+        "runtime": None,
+        "tomato": None,
+        "year": None,
+        "awards": None,
+        "cast": None,
+        "director": None
+    }
+    if external_api == "omdb":
+        omdb = get_omdb_movie(title, year)
+
+        item["title"] = omdb["Title"]
+        item["genre"] = omdb["Genre"]
+        item["imdb"] = omdb["imdbRating"]
+        item["runtime"] = omdb["Runtime"]
+        item["tomato"] = get_rotten_score(omdb)
+        item["year"] = omdb["Year"]
+        item["awards"] = omdb["Awards"]
+        item["cast"] = omdb["Actors"]
+        item["director"] = omdb["Director"]
+    elif external_api == "tmdb":
+        tmdb = get_tmdb_movie(title, year)
+        item["title"] = tmdb["results"][0]['title']
+        item["year"] = tmdb["results"][0]['release_date'].split('-', 1)[0]
+
+        item["genre"] = "unsupported"
+        item["imdb"] = "unsupported"
+        item["runtime"] = "unsupported"
+        item["tomato"] = "unsupported"
+        item["awards"] = "unsupported"
+        item["cast"] = "unsupported"
+        item["director"] = "unsupported"
+
+        genre_ids = tmdb["results"][0]['genre_ids']
+
+    return item
+
+
+def get_rotten_score(item):
+    if item['Ratings'][1]['Source'] == "Rotten Tomatoes":
+        return item['Ratings'][1]['Value']
+    else:
+        return "N/A"
