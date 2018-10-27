950c331a70b74ec2d871d0a876fb7e9972340e78
Mozzo1000 <andreas@simplymozzo.se>
2018-10-27 21:59:05 +0200

    Change API selection field to use dropdown list
    You can now choose from a dropdown list instead of typing in the API you would want to use and risk a typo or wrong entry.

--- web/forms.py
+++ web/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, SelectField
 
 
 class SettingsForm(FlaskForm):
     log_level_field = StringField('Log level')
     log_location_field = StringField('Log location')
     location_field = StringField('Index location')
-    use_external_api_field = StringField('External API')
+    use_external_api_field = SelectField('External API', choices=[('omdb', 'OMDb'), ('tmdb', 'TMDb')])
     omdb_api_key_field = StringField('OMDb API key')
     tmdb_api_key_field = StringField('TMDb API key')
     submit = SubmitField('Save')
