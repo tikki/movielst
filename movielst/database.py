abaa6fe25f198384965799d6b367e4fe96104eb1
Mozzo1000 <andreas@simplymozzo.se>
2018-10-27 13:36:33 +0200

    Add movie description to database
    Closes #26

--- movielst/database.py
+++ movielst/database.py
@@ -6,7 +6,7 @@ from passlib.context import CryptContext
 from .config import *
 
 pwd_context = CryptContext(schemes=['pbkdf2_sha256'], default='pbkdf2_sha256', pbkdf2_sha256__default_rounds=30000)
-LATEST_VERSION = 0
+LATEST_VERSION = 1
 
 
 def connect_db():
@@ -80,8 +80,8 @@ def create_movie_table():
     sql = '''
         CREATE TABLE IF NOT EXISTS movies
             (title TEXT, genre TEXT, imdb FLOAT, runtime TEXT, tomato TEXT, year INT,
-             awards TEXT, cast TEXT, director TEXT, poster TEXT, response BOOLEAN, file_info_name TEXT UNIQUE,
-              file_info_location TEXT, file_info_ext TEXT)
+             awards TEXT, cast TEXT, director TEXT, poster TEXT, description TEXT, response BOOLEAN, 
+             file_info_name TEXT UNIQUE, file_info_location TEXT, file_info_ext TEXT)
     '''
     con = connect_db()
     cur = con.cursor()
@@ -95,22 +95,22 @@ def add_movie(data, force_index):
         sql = '''
             INSERT INTO movies
             (title, genre, imdb, runtime, tomato, year, awards, cast, director, poster, 
-            response, file_info_name, file_info_location, file_info_ext)
-            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            description, response, file_info_name, file_info_location, file_info_ext)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         '''
     elif force_index is False:
         sql = '''
             INSERT OR IGNORE INTO movies
             (title, genre, imdb, runtime, tomato, year, awards, cast, director, poster, 
-            response, file_info_name, file_info_location, file_info_ext)
-            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            description, response, file_info_name, file_info_location, file_info_ext)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         '''
     con = connect_db()
     cur = con.cursor()
     cur.execute(sql, (data['title'], data['genre'], data['imdb'],
                       data['runtime'], data['tomato'], data['year'],
                       data['awards'], data['cast'], data['director'],
-                      data['poster'], data['response'], data['file_info']['name'],
+                      data['poster'], data['description'], data['response'], data['file_info']['name'],
                       data['file_info']['location'], data['file_info']['extension']))
     con.commit()
 
